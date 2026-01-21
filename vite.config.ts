6e55399404f9e7786bd0a4020caa851bc50340ab
gurramdikshareddy <deekshithar.153@gmail.com>
2026-01-21 22:52:17 +0530

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+//hello
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
